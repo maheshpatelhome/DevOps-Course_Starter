e05fb987d730ff8874e052ddbe8da60a528ca7cc
Mahesh Patel <mahesh.patel.home@gmail.com>
2020-10-16 16:50:12 +0100

    Added ability to show a list of To-Do items Added ability to Add To-Do items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
-
+from flask import Flask, render_template, send_from_directory, request
 from todo_app.flask_config import Config
+
+from todo_app.data.session_items  import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    toDoList = get_items()
+    return render_template('index.html', items=toDoList)
+
+@app.route('/', methods=['POST'])
+def addToDoItem():
+    print('item to add = ' + request.form['todoTitle'])
+
+    add_item(request.form['todoTitle']) 
+    toDoList = get_items()
+    return render_template('index.html', items=toDoList)
 
 
 if __name__ == '__main__':
