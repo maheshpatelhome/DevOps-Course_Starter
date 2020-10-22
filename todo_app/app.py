9a4b3c13acd902222670b84643560a4272bfae43
Mahesh Patel <mahesh.patel.home@gmail.com>
2020-10-22 19:50:22 +0100

    chnages following code review

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, redirect
 from todo_app.flask_config import Config
 
 from todo_app.data.session_items  import get_items, add_item
@@ -8,16 +8,13 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    toDoList = get_items()
-    return render_template('index.html', items=toDoList)
+    to_do_list = get_items()
+    return render_template('index.html', items=to_do_list)
 
 @app.route('/', methods=['POST'])
-def addToDoItem():
-    print('item to add = ' + request.form['todoTitle'])
-
-    add_item(request.form['todoTitle']) 
-    toDoList = get_items()
-    return render_template('index.html', items=toDoList)
+def add_to_do_item():
+    add_item(request.form['todoTitle'])
+    return redirect("/")
 
 
 if __name__ == '__main__':
