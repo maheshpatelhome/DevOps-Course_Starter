b366344a77fd542d9da387950a8a6c858250468d
Mahesh Patel <mahesh.patel.home@gmail.com>
2020-11-14 17:31:26 +0000

    fixed incorrect file name

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, send_from_directory, request, redirect
 from todo_app.flask_config import Config
 from todo_app.data.session_items  import get_items, add_item, get_item, save_item
-from todo_app.trello import Trello
+from todo_app.trello_config import TrelloConfig
 
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    config = Trello()
+    config = TrelloConfig()
     print ('running app ' + config.trello_key + ' ' + config.trello_token) 
     to_do_list = sorted(get_items(), key=lambda item: item["status"], reverse=True)
     return render_template('index.html', items=to_do_list)
