4ff88b8c99af0264ef2d237992f124060ff95a8e
Mahesh Patel <mahesh.patel.home@gmail.com>
2020-11-14 17:26:49 +0000

    added ability to mark a task as completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,8 @@
 
 from flask import Flask, render_template, send_from_directory, request, redirect
 from todo_app.flask_config import Config
-from todo_app.data.session_items  import get_items, add_item
-from todo_app.trello_config import TrelloConfig
+from todo_app.data.session_items  import get_items, add_item, get_item, save_item
+from todo_app.trello import Trello
 
 
 app = Flask(__name__)
@@ -10,14 +10,21 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    config = TrelloConfig()
+    config = Trello()
     print ('running app ' + config.trello_key + ' ' + config.trello_token) 
-    to_do_list = get_items()
+    to_do_list = sorted(get_items(), key=lambda item: item["status"], reverse=True)
     return render_template('index.html', items=to_do_list)
 
 @app.route('/', methods=['POST'])
 def add_to_do_item():
     add_item(request.form['todoTitle'])
+    return redirect("/")
+
+@app.route('/completed', methods=['POST'])
+def mark_item_as_completed():
+    item = get_item(request.form["itemId"])
+    item["status"] = "Completed"
+    save_item(item)
     return redirect("/")
 
 if __name__ == '__main__':
